2858aced1416610723967546710e235b15b95f16
serkvasov <m-cody@rambler.ru>
2021-12-13 23:20:54 +0300

    homework3

--- src/test/java/otusAutoQA_HM1/HomeworkTest.java
+++ src/test/java/otusAutoQA_HM1/HomeworkTest.java
@@ -1,81 +1,87 @@
 package otusAutoQA_HM1;
 
 import com.automation.remarks.testng.UniversalVideoListener;
-import com.automation.remarks.video.annotations.Video;
-import config.TestConfig;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Dimension;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.events.EventFiringWebDriver;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
-import java.util.List;
-import java.util.concurrent.TimeUnit;
-
 @Listeners(UniversalVideoListener.class)
 public class HomeworkTest {
-    private EventFiringWebDriver driver;
+
+    private RequestSpecification requestSpecification;
 
     @BeforeClass
     public void setupClass() {
 
-        driver = new WebDriverFactory(TestConfig.getInstance().getBrowser()).createDriver();
-        System.setProperty("video.folder", "target/video");
-        System.setProperty("video.save.mode", "ALL");
-
-        driver.register(new Highlighter());
-        driver.manage().window().setSize(new Dimension(1366, 768));
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-
+        requestSpecification = RestAssured.given().log().all();
+        requestSpecification.baseUri("https://petstore.swagger.io/v2");
     }
 
     @AfterClass
     public void teardown() {
-        if (driver != null) {
-            driver.quit();
-        }
     }
 
     @Test
-    @Video(name = "test")
-    public void test() {
-
-        driver.navigate().to("https://otus.ru/");
-        filterCourses();
-        clickCourse();
-        try {
-            Thread.sleep(3000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+    public void createUser_1() {
+        //Создаем юзера и проверяем статус выполнения запроса
+        requestSpecification.basePath("/user");
+        requestSpecification.contentType("application/json");
+        requestSpecification.body("{\n" +
+                "  \"id\": 0,\n" +
+                "  \"username\": \"userName_1\",\n" +
+                "  \"firstName\": \"firstName_1\",\n" +
+                "  \"lastName\": \"lastName_1\",\n" +
+                "  \"email\": \"email@cucu.ha\",\n" +
+                "  \"password\": \"123456\",\n" +
+                "  \"phone\": \"9857456214\",\n" +
+                "  \"userStatus\": 0\n" +
+                "}");
+        requestSpecification.when().post().then().statusCode(200);
     }
 
-    public void filterCourses() {
-        List<WebElement> webElements = driver.findElements(By.xpath("//*[contains(@class,\"lessons__new-item-title \")]"));
-        webElements.stream().filter(x -> x.getText().contains("QA Automation Engineer")).forEach(x -> System.out.println(x.getText()));
+    @Test
+    public void createUser_2() {
+// Создаем юзера и проверяем, что заголовок должен иметь Content-Type = application/json
+        requestSpecification.basePath("/user");
+        requestSpecification.contentType("application/json");
+        requestSpecification.body("{\n" +
+                "  \"id\": 0,\n" +
+                "  \"username\": \"userName_1\",\n" +
+                "  \"firstName\": \"firstName_1\",\n" +
+                "  \"lastName\": \"lastName_1\",\n" +
+                "  \"email\": \"email@cucu.ha\",\n" +
+                "  \"password\": \"123456\",\n" +
+                "  \"phone\": \"9857456214\",\n" +
+                "  \"userStatus\": 0\n" +
+                "}");
+
+        requestSpecification
+                .when()
+                .post()
+                .then()
+                .header("Content-Type", "application/json");
     }
 
-    public void clickCourse() {
+    @Test
+    public void getUserByName_1() {
+// Проверяем наличие юзера по имени стринг. Проверяем, что в body есть username=string
+        requestSpecification.basePath("/user/string");
+        Response response = requestSpecification.get();
+        Assert.assertEquals(response.getBody().jsonPath().get("username"), "string");
+    }
 
-//        List<WebElement> webElement1 = driver.findElements(By.xpath("//*[@class=\"lessons__new-item-start\"]"));
-//        List<String> list = new ArrayList<>();
-//        webElement1.forEach(x -> list.add((x.getText()).replace("С ", "")));
-//        System.out.println("list " + list);
-//        list.stream().reduce(Integer.MAX_VALUE, (left, right) -> left < right ? left : right);
-                /* Про reduce задачу понял так:
-        нужно со страницы отуса собрать курсы и их даты.
-        Даты отделить от продолжительности курса. Отредьюсить. Потом связать с элементами. Кликнуть по элементу.
-        За это дается 1 балл. Очень сложно. Или я что-то упустил...
-         */
-        WebElement webElement = driver.findElement(By.xpath("//a[@href=\"/promo/qa-auto-java-specialization/\"]"));
-        new Actions(driver)
-                .moveToElement(webElement)
-                .click()
-                .build()
-                .perform();
+    @Test
+    public void getUserByName_2() {
+// Проверяем наличие юзера по имени стринг. Проверяем статус выполнения запроса
+        requestSpecification.basePath("/user/string");
+        Response response = requestSpecification.get();
+        Assert.assertEquals(response.getStatusCode(), 200);
     }
 }
