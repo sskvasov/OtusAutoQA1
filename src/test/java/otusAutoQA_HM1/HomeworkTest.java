2f489f4978d29f5eb53d991faae6f679abb787ee
serkvasov <m-cody@rambler.ru>
2021-12-14 23:54:57 +0300

    homework3

--- src/test/java/otusAutoQA_HM1/HomeworkTest.java
+++ src/test/java/otusAutoQA_HM1/HomeworkTest.java
@@ -2,6 +2,7 @@ package otusAutoQA_HM1;
 
 import com.automation.remarks.testng.UniversalVideoListener;
 
+import com.github.tomakehurst.wiremock.WireMockServer;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
@@ -12,76 +13,130 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.options;
+
 @Listeners(UniversalVideoListener.class)
 public class HomeworkTest {
 
     private RequestSpecification requestSpecification;
+    WireMockServer wireMockServer;
 
     @BeforeClass
     public void setupClass() {
+        wireMockServer = new WireMockServer(options().port(8089));
+        wireMockServer.start();
+        configureFor("localhost", 8089);
+        stubFor(get(urlMatching("/user/get/([0-9]*)"))
+                .willReturn(aResponse()
+                        .withStatus(200)
+                        .withHeader("Content-Type", "application/json")
+                        .withTransformers("response-template")
+                        .withBody("{\n" +
+                                " \"name\":\"Test user\",\n" +
+                                " \"course\":\"QA\",\n" +
+                                "  \"email\":\"test@test.test\"\n" +
+                                "  \"age\": 23\n" +
+                                "}")));
+
+        stubFor(get(urlMatching("/user/get/all"))
+                .willReturn(aResponse()
+                        .withStatus(200)
+                        .withHeader("Content-Type", "application/json")
+                        .withTransformers("response-template")
+                        .withBody("[\n" +
+                                " {\n" +
+                                " \"name\":\"Test user\",\n" +
+                                " \"course\":\"QA\",\n" +
+                                "  \"email\":\"test@test.test\"\n" +
+                                "  \"age\": 23\n" +
+                                " },\n" +
+                                " {\n" +
+                                " \"name\":\"Test user 2\",\n" +
+                                " \"course\":\"SQA\",\n" +
+                                "  \"email\":\"test2@test.ru\"\n" +
+                                "  \"age\": 25\n" +
+                                " }\n" +
+                                "]")));
+
+        stubFor(get(urlMatching("/course/get/all"))
+                .willReturn(aResponse()
+                        .withStatus(200)
+                        .withHeader("Content-Type", "application/json")
+                        .withTransformers("response-template")
+                        .withBody("[\n" +
+                                " {\n" +
+                                "   \"name\":\"QA java\",\n" +
+                                "  \"price\": 15000\n" +
+                                " },\n" +
+                                " {\n" +
+                                "  \"name\":\"Java\",\n" +
+                                "   \"price\": 12000\n" +
+                                " }\n" +
+                                "]")));
+
+
+        stubFor(get(urlMatching("/get/grade"))
+                .willReturn(aResponse()
+                        .withStatus(200)
+                        .withHeader("Content-Type", "application/json")
+                        .withTransformers("response-template")
+                        .withBody("{\n" +
+                                "  \"name\":\"Test user\",\n" +
+                                "  \"score\": 78 \n" +
+                                "}")));
+
 
         requestSpecification = RestAssured.given().log().all();
-        requestSpecification.baseUri("https://petstore.swagger.io/v2");
+        requestSpecification.baseUri("http://localhost:8089/");
     }
 
     @AfterClass
     public void teardown() {
+        wireMockServer.stop();
     }
 
     @Test
-    public void createUser_1() {
+    public void getUser_1() {
+
         //Создаем юзера и проверяем статус выполнения запроса
-        requestSpecification.basePath("/user");
+        requestSpecification.basePath("/user/get/1");
         requestSpecification.contentType("application/json");
-        requestSpecification.body("{\n" +
-                "  \"id\": 0,\n" +
-                "  \"username\": \"userName_1\",\n" +
-                "  \"firstName\": \"firstName_1\",\n" +
-                "  \"lastName\": \"lastName_1\",\n" +
-                "  \"email\": \"email@cucu.ha\",\n" +
-                "  \"password\": \"123456\",\n" +
-                "  \"phone\": \"9857456214\",\n" +
-                "  \"userStatus\": 0\n" +
-                "}");
-        requestSpecification.when().post().then().statusCode(200);
+        requestSpecification.when().get().then().statusCode(200);
+        Response response = requestSpecification.when().get();
+        response.body().prettyPrint();
     }
 
     @Test
-    public void createUser_2() {
-// Создаем юзера и проверяем, что заголовок должен иметь Content-Type = application/json
-        requestSpecification.basePath("/user");
+    public void getAllUsers() {
+
+        //Создаем юзера и проверяем статус выполнения запроса
+        requestSpecification.basePath("/user/get/all");
         requestSpecification.contentType("application/json");
-        requestSpecification.body("{\n" +
-                "  \"id\": 0,\n" +
-                "  \"username\": \"userName_1\",\n" +
-                "  \"firstName\": \"firstName_1\",\n" +
-                "  \"lastName\": \"lastName_1\",\n" +
-                "  \"email\": \"email@cucu.ha\",\n" +
-                "  \"password\": \"123456\",\n" +
-                "  \"phone\": \"9857456214\",\n" +
-                "  \"userStatus\": 0\n" +
-                "}");
-
-        requestSpecification
-                .when()
-                .post()
-                .then()
-                .header("Content-Type", "application/json");
+        requestSpecification.when().get().then().statusCode(200);
+        Response response = requestSpecification.when().get();
+        response.body().prettyPrint();
     }
 
     @Test
-    public void getUserByName_1() {
-// Проверяем наличие юзера по имени стринг. Проверяем, что в body есть username=string
-        requestSpecification.basePath("/user/string");
-        Response response = requestSpecification.get();
-        Assert.assertEquals(response.getBody().jsonPath().get("username"), "string");
+    public void getAllCourses() {
+
+        //Создаем юзера и проверяем статус выполнения запроса
+        requestSpecification.basePath("/course/get/all");
+        requestSpecification.contentType("application/json");
+        requestSpecification.when().get().then().statusCode(200);
+        Response response = requestSpecification.when().get();
+        response.body().prettyPrint();
     }
 
     @Test
-    public void getUserByName_2() {
-// Проверяем наличие юзера по имени стринг. Проверяем статус выполнения запроса
-        requestSpecification.basePath("/user/string");
-        Response response = requestSpecification.get();
-        Assert.assertEquals(response.getStatusCode(), 200);
+    public void getGrade() {
+
+        //Создаем юзера и проверяем статус выполнения запроса
+        requestSpecification.basePath("/get/grade");
+        requestSpecification.contentType("application/json");
+        requestSpecification.when().get().then().statusCode(200);
+        Response response = requestSpecification.when().get();
+        response.body().prettyPrint();
     }
 }
